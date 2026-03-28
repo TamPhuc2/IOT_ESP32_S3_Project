78ad0e69c09d792a14d8de0595f91eede3facc8d
TamPhuc2 <tntamphuc@gmail.com>
2026-03-28 14:16:09 +0700

    Task 1, 2, 3 done

--- src/global.cpp
+++ src/global.cpp
@@ -1,6 +1,4 @@
 #include "global.h"
-float glob_temperature = 0;
-float glob_humidity = 0;
 
 String WIFI_SSID;
 String WIFI_PASS;
@@ -13,4 +11,5 @@ String password = "12345678";
 String wifi_ssid = "abcde";
 String wifi_password = "123456789";
 boolean isWifiConnected = false;
+
 SemaphoreHandle_t xBinarySemaphoreInternet = xSemaphoreCreateBinary();
