3592735eb1bd00563e759f298fb4dc92d3bc4cfb
TamPhuc2 <tntamphuc@gmail.com>
2026-04-17 14:17:29 +0700

    fixed all STA and AP mode

--- src/main.cpp
+++ src/main.cpp
@@ -56,6 +56,7 @@ void setup()
   xTaskCreate(main_server_task, "Task Main Server" ,8192, (void*)&sysHandles, 2, NULL);
   xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,(void*)&sysHandles  ,2 , NULL);
   xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,(void*)&sysHandles  ,2 , NULL);
+  
   // xTaskCreate(Task_Toogle_BOOT, "Task_Toogle_BOOT", 4096, NULL, 2, NULL);
 }
 
